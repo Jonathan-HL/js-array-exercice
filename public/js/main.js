8709cbfce28493ce4841dbb398be0af0030a12cb
Jonathan <jonxplod@outlook.fr>
2021-10-04 22:14:56 +0200

    exercice array 7

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+// Exo 1
+let tableau = ["phil","here","ayhan","jnt"];
+console.log(tableau);
+console.log(tableau[3]);
+console.log(tableau.length);
+// Exo 2
+let tableau2 = ["jnt"];
+console.log(tableau2);
+tableau2.push("tnj");
+console.log(tableau2);
+// Exo 3
+let classeCoding = ["Ayhan"]
+classeCoding.push("Benedicte")
+console.log(classeCoding);
+classeCoding.push("Louise","Jamila");
+console.log(classeCoding);
+classeCoding.push("Sebastien","Adame");
+console.log(classeCoding);
+classeCoding.push("Dawid","Malo");
+console.log(classeCoding);
+classeCoding.push("alex")
+console.log(classeCoding);
+classeCoding.pop();
+console.log(classeCoding);
+classeCoding.push("Maxence","Mohammed");
+console.log(classeCoding);
+classeCoding.push("Saliou");
+console.log(classeCoding);
+classeCoding.push("Zulma","Maximilien");
+console.log(classeCoding);
+classeCoding.push("Natchez");
+console.log(classeCoding);
+classeCoding.push("Zakaria","Ayoub");
+console.log(classeCoding);
+classeCoding.splice(14,16);
+console.log(classeCoding);
+classeCoding.push("Achraf");
+console.log(classeCoding);
+classeCoding.push("Abdellah");
+console.log(classeCoding);
+
+
